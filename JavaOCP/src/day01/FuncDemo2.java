0edf259ee4c641f5a224afe3f856ec5c3b8cdc69
teacher <teacher@BE-403->
2024-05-10 21:40:27 +0800

    java

--- JavaOCP/src/day01/FuncDemo2.java
+++ JavaOCP/src/day01/FuncDemo2.java
@@ -11,8 +11,19 @@ public class FuncDemo2 {
 		// 請設計一個方法來實現
 		// 方法名稱: getDistance
 		int x1=10, y1=20, x2=-15, y2=50;
+		double distance1 = getDistance(x1, x2, y1, y2);
+		System.out.println(distance1);
 		
-
+		int a1=0, b1=5, a2=5, b2=0;
+		double distance2 = getDistance(a1, b1, a2, b2);
+		System.out.println(distance2);
+	}
+	
+	private static double getDistance(int x1, int x2, int y1, int y2) {
+		double dx = Math.pow(x1-x2, 2);
+		double dy = Math.pow(y1-y2, 2);
+		double distance = Math.sqrt(dx + dy);
+		return distance;
 	}
 
 }
