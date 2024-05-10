bedaf4c5a236ded647d2d3839bec674b51ad8543
teacher <teacher@BE-403->
2024-05-10 21:17:26 +0800

    java

--- JavaOCP/src/day01/FuncDemo2.java
+++ JavaOCP/src/day01/FuncDemo2.java
@@ -3,7 +3,15 @@ package day01;
 public class FuncDemo2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Math.pow(4, 2) = 16.0 指數
+		// Math.sqrt(16) = 4.0 開根號
+		// 有 a, b 兩點
+		// a(x1, y1), b(x2, y2)
+		// 求 ab 二點間的距離
+		// 請設計一個方法來實現
+		// 方法名稱: getDistance
+		int x1=10, y1=20, x2=-15, y2=50;
+		
 
 	}
 
