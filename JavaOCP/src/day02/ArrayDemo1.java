531f62c05bf4684bedb12764ecb8e1a17b151a7a
teacher <teacher@BE-403->
2024-05-13 21:12:27 +0800

    java

---
+++ JavaOCP/src/day02/ArrayDemo1.java
@@ -0,0 +1,19 @@
+package day02;
+
+import java.util.Arrays;
+
+public class ArrayDemo1 {
+
+	public static void main(String[] args) {
+		int[] rooms = new int[5];
+		// [I@1ee0005
+		// [ => 一維陣列
+		// I => int
+		// @ => At(在)
+		// 1ee0005 => 該物件的雜湊值
+		System.out.println(rooms);
+		System.out.println(rooms[0] + ", " + rooms[1] + ", " + rooms[2] + ", " + rooms[3] + ", " + rooms[4]);
+		System.out.println(Arrays.toString(rooms));
+	}
+
+}
