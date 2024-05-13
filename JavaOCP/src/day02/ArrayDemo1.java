2be97f61b78132645c1f4799f6374db8e069ba5e
teacher <teacher@BE-403->
2024-05-13 21:18:41 +0800

    java

--- JavaOCP/src/day02/ArrayDemo1.java
+++ JavaOCP/src/day02/ArrayDemo1.java
@@ -12,9 +12,19 @@ public class ArrayDemo1 {
 		// @ => At(在)
 		// 1ee0005 => 該物件的雜湊值
 		System.out.println(rooms);
-		System.out.println(rooms[0] + ", " + rooms[1] + ", " + rooms[2] + ", " + rooms[3] + ", " + rooms[4]);
-		System.out.println(Arrays.toString(rooms));
 		
+		System.out.println("房客入住前:");
+		//System.out.println(rooms[0] + ", " + rooms[1] + ", " + rooms[2] + ", " + rooms[3] + ", " + rooms[4]);
+		System.out.println(Arrays.toString(rooms));
+		// 房客入住
+		rooms[0] = 4;
+		rooms[1] = 2;
+		rooms[2] = 3;
+		rooms[3] = 6;
+		rooms[4] = 1;
+		
+		System.out.println("房客入住後:");
+		System.out.println(Arrays.toString(rooms));
 		
 	}
 
