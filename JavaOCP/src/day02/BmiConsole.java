e522614bb0c33855bf04e0d9102207843bc893fd
teacher <teacher@BE-403->
2024-05-13 19:19:02 +0800

    java

--- JavaOCP/src/day02/BmiConsole.java
+++ JavaOCP/src/day02/BmiConsole.java
@@ -1,17 +1,22 @@
 package day02;
 
+// 靜態 import static
+import static day02.BmiUtil.getBmi;
+import static day02.BmiUtil.getResult;
+import static day02.BmiUtil.printBmiInfo;
+
 public class BmiConsole {
 
 	public static void main(String[] args) {
 		// 使用 BmiUtil
 		double h = 170.0;
 		double w = 60.0;
-		double bmi = BmiUtil.getBmi(h, w);
-		String result = BmiUtil.getResult(bmi);
+		double bmi = getBmi(h, w);
+		String result = getResult(bmi);
 		System.out.println(bmi);
 		System.out.println(result);
 		//-------------------------------------
-		BmiUtil.printBmiInfo(h, w);
+		printBmiInfo(h, w);
 	}
 
 }
