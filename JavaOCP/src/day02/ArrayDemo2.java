373374d65cdc0df4ca7b94979b91850733e9afeb
teacher <teacher@BE-403->
2024-05-13 21:36:09 +0800

    java

--- JavaOCP/src/day02/ArrayDemo2.java
+++ JavaOCP/src/day02/ArrayDemo2.java
@@ -10,7 +10,17 @@ public class ArrayDemo2 {
 		int[] rooms = new int[]{4, 2, 3, 6, 1};
 		System.out.println("房客入住後:");
 		System.out.println(Arrays.toString(rooms));
+		System.out.printf("房間數: %d 間%n", rooms.length);
+		// 請計算房間總人數與平均人數
+		//int sum = rooms[0] + rooms[1] + rooms[2] + rooms[3] + rooms[4];
+		int sum = 0;
+		for(int i=0;i<rooms.length;i++) {
+			//sum = sum + rooms[i]; // 累加
+			sum += rooms[i]; // 累加
+		}
+		double avg = (double)sum/rooms.length;
 		
+		System.out.printf("房間總人數:%d 平均人數:%.1f%n", sum, avg);
 	}
 
 }
