3eb3dc9712c26ba308ae8f921caca26e83562629
teacher <teacher@BE-403->
2024-05-13 19:22:00 +0800

    bmi

--- JavaOCP/src/day02/BmiConsole.java
+++ JavaOCP/src/day02/BmiConsole.java
@@ -1,5 +1,6 @@
 package day02;
 
+import java.util.Scanner;
 // 靜態 import static
 import static day02.BmiUtil.getBmi;
 import static day02.BmiUtil.getResult;
@@ -8,9 +9,11 @@ import static day02.BmiUtil.printBmiInfo;
 public class BmiConsole {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("請輸入身高與體重(例如: 170 60):");
 		// 使用 BmiUtil
-		double h = 170.0;
-		double w = 60.0;
+		double h = scanner.nextDouble();
+		double w = scanner.nextDouble();
 		double bmi = getBmi(h, w);
 		String result = getResult(bmi);
 		System.out.println(bmi);
