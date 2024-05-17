9e74b59ac7f59afe2239ce433939f9370b54bd65
teacher <teacher@BE-403->
2024-05-17 19:59:33 +0800

    java

---
+++ JavaOCP/src/day03/ArrayDemo7.java
@@ -0,0 +1,12 @@
+package day03;
+
+public class ArrayDemo7 {
+	public static void main(String[] args) {
+		// 飲料特價買一送一
+		String[] names = {"Fin運動飲料", "柳橙汁(大)"};
+		int[] prices = {25, 75};
+		int[] amount = {4, 2};
+		// 請計算本次消費金額 = ?
+		
+	}
+}
