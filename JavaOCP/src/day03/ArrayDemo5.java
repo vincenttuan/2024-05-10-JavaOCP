cb28a2727cdd5b63199056a16d1d540a13151fc3
teacher <teacher@BE-403->
2024-05-17 19:25:42 +0800

    drink

---
+++ JavaOCP/src/day03/ArrayDemo5.java
@@ -0,0 +1,23 @@
+package day03;
+
+public class ArrayDemo5 {
+
+	public static void main(String[] args) {
+		// 飲料,價格,數量
+		String[] names = {"可樂", "紅茶", "咖啡"};
+		int[] prices   = {25, 15, 45};
+		int[] amounts  = {3, 5, 9};
+		// 請印出   
+		// 結帳明細
+		// -------------
+		// 可樂 $25 x 3
+		// 紅茶 $15 x 5
+		// 咖啡 $45 x 9
+		// -------------
+		
+		
+		
+
+	}
+
+}
