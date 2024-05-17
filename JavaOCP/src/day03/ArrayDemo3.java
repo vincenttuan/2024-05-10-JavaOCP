c7a67a8d31bfe379fde2c443b7683ac90160e5dc
teacher <teacher@BE-403->
2024-05-17 18:55:54 +0800

    java

--- JavaOCP/src/day03/ArrayDemo3.java
+++ JavaOCP/src/day03/ArrayDemo3.java
@@ -5,11 +5,18 @@ public class ArrayDemo3 {
 	public static void main(String[] args) {
 		// Java 一維陣列
 		// 房間
-		int[] rooms = {5, 4, 2, 0, 3};
+		int[] rooms = {5, 4, 7, 0, 3};
 		System.out.printf("房間數: %d%n", rooms.length);
+		// 利用 for-loop 來輪詢陣列資料
+		// i從0~<5
 		for(int i=0;i<rooms.length;i++) {
 			System.out.printf("%d 號房有 %d 人%n", i, rooms[i]);
 		}
+		// i從4~0
+		
+		
+		// 計算總人數
+		
 	}
 
 }
