765ef8c6879d1d3f896b48e3083df875c37b819d
teacher <teacher@BE-403->
2024-05-17 19:08:45 +0800

    scores

---
+++ JavaOCP/src/day03/ArrayDemo4.java
@@ -0,0 +1,12 @@
+package day03;
+
+public class ArrayDemo4 {
+	public static void main(String[] args) {
+		int[] scores   = {85, 70, 100, 40, 60};
+		String[] names = {"國", "英", "數", "自", "社"};
+		
+		// 請印出 國=85, 英=70, 數=100, 自=40, 社=60
+		
+		
+	}
+}
