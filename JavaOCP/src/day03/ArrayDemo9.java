c255cfa673dd63a29a8826cdf5e448227d43b82c
teacher <teacher@BE-403->
2024-05-17 21:12:45 +0800

    java

---
+++ JavaOCP/src/day03/ArrayDemo9.java
@@ -0,0 +1,13 @@
+package day03;
+
+public class ArrayDemo9 {
+	public static void main(String[] args) {
+		int cash = 100;
+		String[] names = {"養樂多", "金蘋果"};
+		int[] prices = {12, 15};
+		int[] amounts = {4, 3};
+		// 飲料全部買一送一
+		// 請問要花多少錢 ? 要找多少錢 ? 如何找 ? (硬幣個數)
+		
+ 	}
+}
