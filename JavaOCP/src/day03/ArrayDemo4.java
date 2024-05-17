3f18ae31f3a3a38e05470e7f8cddc9cc4912cbb4
teacher <teacher@BE-403->
2024-05-17 19:18:18 +0800

    scores

--- JavaOCP/src/day03/ArrayDemo4.java
+++ JavaOCP/src/day03/ArrayDemo4.java
@@ -6,7 +6,12 @@ public class ArrayDemo4 {
 		String[] names = {"國", "英", "數", "自", "社"};
 		
 		// 請印出 國=85, 英=70, 數=100, 自=40, 社=60
-		
+		for(int i=0, lens=scores.length;i<lens;i++) {
+			System.out.printf("%s=%d", names[i], scores[i]);
+			if(i < scores.length - 1) {
+				System.out.print(", ");
+			}
+		}
 		
 	}
 }
