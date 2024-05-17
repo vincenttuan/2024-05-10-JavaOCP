fb0b35054f7b1bf36eca40b431ead472b334ffae
teacher <teacher@BE-403->
2024-05-17 20:34:25 +0800

    java

--- JavaOCP/src/day03/ArrayDemo7.java
+++ JavaOCP/src/day03/ArrayDemo7.java
@@ -3,10 +3,14 @@ package day03;
 public class ArrayDemo7 {
 	public static void main(String[] args) {
 		// 飲料特價買一送一
-		String[] names = {"Fin運動飲料", "柳橙汁(大)"};
-		int[] prices = {25, 75};
-		int[] amount = {4, 2};
+		String name = "Fin運動飲料";
+		int price = 25;
+		int amount = 4;
 		// 請計算本次消費金額 = ?
+		int paidAmount = amount/2 + amount%2;
+		int subtotal = paidAmount * price;
+		System.out.println(paidAmount);
+		System.out.println(subtotal);
 		
 	}
 }
