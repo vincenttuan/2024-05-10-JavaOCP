0913cd25c19821167e7c86a12c108ef4c1a1b7e1
teacher <teacher@BE-403->
2024-05-20 19:09:19 +0800

    java

---
+++ JavaOCP/src/day04/ArrayFunc2.java
@@ -0,0 +1,11 @@
+package day04;
+
+//陣列與自訂函式(改良 ArrayFunc1.java)
+public class ArrayFunc2 {
+	public static void main(String[] args) {
+		int[] scoreA = {100, 65, 73, 85, 92};
+		int[] scoreB = {56, 65, 55, 66, 60};
+		
+		// 計算 A 與 B 的總分與平均 = ?
+	}
+}
