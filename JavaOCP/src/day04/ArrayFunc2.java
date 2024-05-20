0850964d5fb3bf89332c7c3244d5200fb494529a
teacher <teacher@BE-403->
2024-05-20 19:14:08 +0800

    java

--- JavaOCP/src/day04/ArrayFunc2.java
+++ JavaOCP/src/day04/ArrayFunc2.java
@@ -8,4 +8,17 @@ public class ArrayFunc2 {
 		
 		// 計算 A 與 B 的總分與平均 = ?
 	}
+	
+	// 計算總和與平均的方法
+	// 帶入"分數陣列"與"班級名稱"
+	public static void calcSumAndAvg(int[] scores, String className) {
+		int sum = 0;
+		double avg = 0.0;
+		int lens = scores.length;
+		for(int i=0;i<lens;i++) {
+			sum += scores[i];
+		}
+		avg = (double)sum / lens;
+		System.out.printf("%s 班總分 %d 平均 %.1f%n", className, sum, avg);
+	}
 }
