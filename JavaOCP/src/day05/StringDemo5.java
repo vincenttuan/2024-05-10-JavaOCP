c9f23096e5ca6e54f5b529bf123f5d6d81459c41
teacher <teacher@BE-403->
2024-05-24 19:53:21 +0800

    java

--- JavaOCP/src/day05/StringDemo5.java
+++ JavaOCP/src/day05/StringDemo5.java
@@ -5,10 +5,18 @@ public class StringDemo5 {
 		StringBuilder msg = new StringBuilder("Java");
 		append(msg);
 		System.out.println(msg);
+		//--------------------------------------------
+		String data = "Java";
+		add(data);
+		System.out.println(data);
 	}
 	
 	public static void append(StringBuilder sb) {
 		sb.append("11");
+	}
+	
+	public static void add(String s) {
+		s = s.concat("11"); // s + "11";
 	}
 	
 }
