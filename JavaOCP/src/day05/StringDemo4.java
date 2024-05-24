7a781dbc8cc27e9fdf79c0f0b2f880282177c781
teacher <teacher@BE-403->
2024-05-24 19:34:56 +0800

    java

---
+++ JavaOCP/src/day05/StringDemo4.java
@@ -0,0 +1,14 @@
+package day05;
+
+public class StringDemo4 {
+	
+	public static void main(String[] args) {
+		String s1 = "she sell sea shell on the sea shore";
+		System.out.println(s1);
+		
+		s1 = s1.replace("sell", "buy");
+		System.out.println(s1);
+		
+	}
+	
+}
