b730bfcc376afc598f6a68f395a15ada280a7f49
teacher <teacher@BE-403->
2024-05-24 20:55:23 +0800

    java

--- JavaOCP/src/day05/StringDemo6.java
+++ JavaOCP/src/day05/StringDemo6.java
@@ -11,6 +11,20 @@ public class StringDemo6 {
 		char gender = id.charAt(1); // 1
 		System.out.println(area);
 		System.out.println(gender);
+		
+		int num = (1*1 + 0*9) 
+				+ (id.charAt(1)-48) * 8
+				+ (id.charAt(2)-48) * 7
+				+ (id.charAt(3)-48) * 6
+				+ (id.charAt(4)-48) * 5
+				+ (id.charAt(5)-48) * 4
+				+ (id.charAt(6)-48) * 3
+				+ (id.charAt(7)-48) * 2
+				+ (id.charAt(8)-48) * 1
+				+ (id.charAt(9)-48) * 1;
+		
+		System.out.println(num);
+		System.out.println(num % 10 == 0);
 	}
 
 }
