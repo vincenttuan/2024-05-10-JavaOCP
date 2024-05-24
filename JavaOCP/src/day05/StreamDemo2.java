4c6ca354d2c85bec79f6fc364c9787a8ea55c102
teacher <teacher@BE-403->
2024-05-24 21:41:37 +0800

    java

--- JavaOCP/src/day05/StreamDemo2.java
+++ JavaOCP/src/day05/StreamDemo2.java
@@ -1,5 +1,7 @@
 package day05;
 
+import java.util.Arrays;
+
 public class StreamDemo2 {
 	public static void main(String[] args) {
 		// Stream 資料串流
@@ -12,5 +14,16 @@ public class StreamDemo2 {
 			}
 		}
 		System.out.println();
+		// 2. for-Each
+		for(int score : scores) {
+			if(score >= 60) { // 過濾條件 filter
+				System.out.printf("%d ", score);
+			}
+		}
+		System.out.println();
+		// 3. 使用 Arrays.stream()
+		Arrays.stream(scores)
+			  .filter(score -> score >= 60) // 過濾條件 filter
+			  .forEach(score -> System.out.printf("%d ", score));
 	}
 }
