48c781427445bb641bfce52c71b2ee2e286b36ca
teacher <teacher@BE-403->
2024-05-24 19:21:27 +0800

    java

--- JavaOCP/src/day05/StringDemo2.java
+++ JavaOCP/src/day05/StringDemo2.java
@@ -6,7 +6,7 @@ public class StringDemo2 {
 		String s2 = "Java";
 		System.out.println(s1 == s2);
 		
-		s1 = s1.intern();
+		s1 = s1.intern(); // 將字串放到 String pool 中
 		System.out.println(s1 == s2);
 		
 	}
