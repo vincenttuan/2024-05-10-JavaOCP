290e19c66000d57486cbe9c12629f3ce381adbe1
teacher <teacher@BE-403->
2024-05-24 21:23:01 +0800

    java

---
+++ JavaOCP/src/day05/StreamDemo1.java
@@ -0,0 +1,21 @@
+package day05;
+
+public class StreamDemo1 {
+
+	public static void main(String[] args) {
+		// Stream 資料串流
+		int[] scores = {100, 50, 70, 45, 90};
+		// 1.利用 for-loop 將元素資料逐筆印出
+		for(int i=0;i<scores.length;i++) {
+			System.out.printf("%d ", scores[i]);
+		}
+		System.out.println(); // 換行
+		// 2.利用 for-each(for/in) 將元素資料逐筆印出
+		for(int score : scores) {
+			System.out.printf("%d ", score);
+		}
+		System.out.println(); // 換行
+		
+	}
+
+}
