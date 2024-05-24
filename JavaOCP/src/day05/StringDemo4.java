2e36bab61d4b269849f3560a4aaea1dddca710a2
teacher <teacher@BE-403->
2024-05-24 19:44:46 +0800

    java

--- JavaOCP/src/day05/StringDemo4.java
+++ JavaOCP/src/day05/StringDemo4.java
@@ -3,11 +3,19 @@ package day05;
 public class StringDemo4 {
 	
 	public static void main(String[] args) {
+		// String 不可變字串
 		String s1 = "she sell sea shell on the sea shore";
 		System.out.println(s1);
 		
 		s1 = s1.replace("sell", "buy");
 		System.out.println(s1);
+		
+		//---------------------------------------------------
+		// StringBuilder 可變字串
+		StringBuilder s2 = new StringBuilder("she sell sea shell on the sea shore");
+		System.out.println(s2);
+		s2 = s2.replace(4, 8, "Buy");
+		System.out.println(s2);
 		
 	}
 	
