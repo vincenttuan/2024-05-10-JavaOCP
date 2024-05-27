14824c687ba869e87e4a9be0cdf0b79ea29b87b2
teacher <teacher@BE-403->
2024-05-27 18:52:00 +0800

    java

--- JavaOCP/src/day06/StreamDemo3.java
+++ JavaOCP/src/day06/StreamDemo3.java
@@ -4,9 +4,21 @@ public class StreamDemo3 {
 
 	public static void main(String[] args) {
 		int[] scores = {100, 45, 80, -1, 90, 55};
-		// 請印出所有合理的分數( score >= 0), 並計算總分與平均 
-		
-
+		// 請印出所有合理的分數( score >= 0), 並計算總分,平均,最高分與最低分 
+		// for-each
+		for(int score : scores) {
+			if(score >= 0) {
+				System.out.printf("%d ", score);
+			}
+		}
+		System.out.println();
+		for(int score : scores) {
+			if(score < 0) {
+				continue;
+			}
+			System.out.printf("%d ", score);
+		}
+		System.out.println();
 	}
 
 }
