ba7bccfa607ee591920622c2b1486cbbba6652d2
teacher <teacher@BE-403->
2024-05-27 18:42:50 +0800

    java

---
+++ JavaOCP/src/day06/StreamDemo3.java
@@ -0,0 +1,12 @@
+package day06;
+
+public class StreamDemo3 {
+
+	public static void main(String[] args) {
+		int[] scores = {100, 45, 80, -1, 90, 55};
+		// 請印出所有合理的分數( >= 0), 並計算總分與平均 
+		
+
+	}
+
+}
