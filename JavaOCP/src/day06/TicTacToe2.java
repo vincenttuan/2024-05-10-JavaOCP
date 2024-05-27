86b84e30b6950ee2cd086e0577efe2ac1343a26a
teacher <teacher@BE-403->
2024-05-27 21:15:19 +0800

    java

---
+++ JavaOCP/src/day06/TicTacToe2.java
@@ -0,0 +1,12 @@
+package day06;
+
+public class TicTacToe2 {
+	public static void main(String[] args) {
+		char[] board = {'X', 'O', ' ', 'O', 'X', 'O', ' ', 'X', ' '};
+		// 請問 board[?] = 'X'; 'X' 會勝利
+		// 請印出 board 井字遊戲棋盤樣式
+		
+		
+		
+	}
+}
