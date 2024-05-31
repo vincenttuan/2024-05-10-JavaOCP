4969bfa423bb70d6d90cdf9d5a7d7759d64b4410
teacher <teacher@BE-403->
2024-05-31 19:55:23 +0800

    java

---
+++ JavaOCP/src/day07/DrinkMain.java
@@ -0,0 +1,29 @@
+package day07;
+
+public class DrinkMain {
+
+	public static void main(String[] args) {
+		Drink drink1 = new Drink();
+		drink1.name = "珍奶";
+		drink1.amount = 1;
+		drink1.price = 70;
+		
+		Drink drink2 = new Drink();
+		drink2.name = "青茶";
+		drink2.amount = 10;
+		drink2.price = 40;
+		
+		Drink drink3 = new Drink();
+		drink3.name = "紅茶";
+		drink3.amount = 20;
+		drink3.price = 25;
+		
+		Drink drink4 = new Drink();
+		drink4.name = "熱可可";
+		drink4.amount = 15;
+		drink4.price = 55;
+		
+
+	}
+
+}
