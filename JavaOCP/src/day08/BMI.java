7576153cf260d96e65212c742ffa9a237e568e9d
teacher <teacher@BE-403->
2024-06-03 21:30:51 +0800

    java

--- JavaOCP/src/day08/BMI.java
+++ JavaOCP/src/day08/BMI.java
@@ -18,9 +18,20 @@ public class BMI {
 	}
 	
 	void printBmi() {
-		double bmi = weight / Math.pow(height/100, 2);
-		String result = bmi > 23 ? "過重" : bmi <= 18 ? "過輕" : "正常";
+		double bmi = getBmi();
+		String result = getResult(bmi);
 		System.out.printf("姓名: %s 身高: %.1f 體重: %.1f BMI: %.2f 診斷: %s%n", 
 				name, height, weight, bmi, result);
 	}
+	
+	private double getBmi() {
+		double bmi = weight / Math.pow(height/100, 2);
+		return bmi;
+	}
+	
+	private String getResult(double bmi) {
+		String result = bmi > 23 ? "過重" : bmi <= 18 ? "過輕" : "正常";
+		return result;
+	}
+	
 }
