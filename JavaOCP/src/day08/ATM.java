83e418754412abca7cc0bf101795f44eb9f37c63
teacher <teacher@BE-403->
2024-06-03 19:23:32 +0800

    java

---
+++ JavaOCP/src/day08/ATM.java
@@ -0,0 +1,21 @@
+package day08;
+
+public class ATM {
+	public static void main(String[] args) {
+		Account johnAct = new Account(); // John 帳戶
+		johnAct.name = "John";
+		Account maryAct = new Account(); // Mary 帳戶
+		maryAct.name = "Mary";
+		
+		johnAct.deposit(5000);
+		johnAct.printBalance();
+		
+		johnAct.withdraw(2000);
+		johnAct.printBalance();
+		
+		johnAct.transfer(2500, maryAct);
+		johnAct.printBalance();
+		maryAct.printBalance();
+		
+	}
+}
