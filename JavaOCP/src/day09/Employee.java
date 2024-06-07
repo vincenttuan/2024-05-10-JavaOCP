f4242108e089e2debc35e2ea846cc6b4ade5da41
teacher <teacher@BE-403->
2024-06-07 19:30:44 +0800

    java

--- JavaOCP/src/day09/Employee.java
+++ JavaOCP/src/day09/Employee.java
@@ -5,37 +5,33 @@ public class Employee {
 	private String name; // 姓名
 	private int salary;  // 薪資 
 	
-	// 建構子
 	public Employee() {
-		//System.out.println("呼叫  Employee 建構子");
 	}
 	
-	// 建構子
 	public Employee(String name, int salary) {
-		//System.out.println("呼叫  Employee 建構子");
 		this.name = name;
 		this.salary = salary;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
-	
-	public int getSalary() {
-		return salary;
-	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
+	public int getSalary() {
+		return salary;
+	}
+
 	public void setSalary(int salary) {
 		this.salary = salary;
 	}
-	
-	// 將物件的屬性資料輸出
+
+	@Override
 	public String toString() {
-		return String.format("name: %s salary: %d", name, salary);
+		return "Employee [name=" + name + ", salary=" + salary + "]";
 	}
 	
 	
