d0733098327c44bfd9dfdc66f62149baee587c76
teacher <teacher@BE-403->
2024-06-07 20:07:41 +0800

    java

--- JavaOCP/src/day09/Book.java
+++ JavaOCP/src/day09/Book.java
@@ -5,26 +5,31 @@ public class Book {
 	private String author;
 	private int totalPages;
 	
-	public Book() {
+	// 建構子初始區(所有建構子都會執行的地區)
+	{
 		System.out.println("歡迎來閱讀本書");
+	}
+	
+	public Book() {
+		//System.out.println("歡迎來閱讀本書");
 		name = "unknow";
 		author = "unknow";
 		totalPages = -1;
 	}
 	
 	public Book(String name) {
-		System.out.println("歡迎來閱讀本書");
+		//System.out.println("歡迎來閱讀本書");
 		this.name = name;
 	}
 	
 	public Book(String name, String author) {
-		System.out.println("歡迎來閱讀本書");
+		//System.out.println("歡迎來閱讀本書");
 		this.name = name;
 		this.author = author;
 	}
 	
 	public Book(String name, String author, int totalPages) {
-		System.out.println("歡迎來閱讀本書");
+		//System.out.println("歡迎來閱讀本書");
 		this.name = name;
 		this.author = author;
 		this.totalPages = totalPages;
