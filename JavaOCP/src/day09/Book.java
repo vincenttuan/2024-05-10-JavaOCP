5b09947eb55c48f6f928b75f8145269db82c2cd6
teacher <teacher@BE-403->
2024-06-07 20:04:04 +0800

    java

--- JavaOCP/src/day09/Book.java
+++ JavaOCP/src/day09/Book.java
@@ -6,19 +6,25 @@ public class Book {
 	private int totalPages;
 	
 	public Book() {
-		
+		System.out.println("歡迎來閱讀本書");
+		name = "unknow";
+		author = "unknow";
+		totalPages = -1;
 	}
 	
 	public Book(String name) {
+		System.out.println("歡迎來閱讀本書");
 		this.name = name;
 	}
 	
 	public Book(String name, String author) {
+		System.out.println("歡迎來閱讀本書");
 		this.name = name;
 		this.author = author;
 	}
 	
 	public Book(String name, String author, int totalPages) {
+		System.out.println("歡迎來閱讀本書");
 		this.name = name;
 		this.author = author;
 		this.totalPages = totalPages;
