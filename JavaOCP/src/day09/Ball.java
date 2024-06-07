c5693fab38af609eec1e278dfa2eece6615b9c32
teacher <teacher@BE-403->
2024-06-07 19:55:28 +0800

    java

---
+++ JavaOCP/src/day09/Ball.java
@@ -0,0 +1,15 @@
+package day09;
+
+public class Ball {
+	private int price = 200;
+	
+	public Ball() {
+		this.price = 100;
+	}
+
+	@Override
+	public String toString() {
+		return "Ball [price=" + price + "]";
+	}
+	
+}
