d0b3a75349c1928b538d6c7d57a130d2dc7e1585
teacher <teacher@BE-403->
2024-06-07 19:57:41 +0800

    java

--- JavaOCP/src/day09/Ball.java
+++ JavaOCP/src/day09/Ball.java
@@ -4,12 +4,19 @@ public class Ball {
 	private int price = 200;
 	
 	public Ball() {
+		// price = 200;
 		this.price = 100;
 	}
 
 	@Override
 	public String toString() {
 		return "Ball [price=" + price + "]";
+	}
+	
+	// 主程式
+	public static void main(String[] args) {
+		Ball ball = new Ball();
+		System.out.println(ball);
 	}
 	
 }
