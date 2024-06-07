2d12820835fb277d279dafa95624fdec7d37b305
teacher <teacher@BE-403->
2024-06-07 19:02:48 +0800

    java

--- JavaOCP/src/day09/Employee.java
+++ JavaOCP/src/day09/Employee.java
@@ -2,9 +2,20 @@ package day09;
 
 // 員工
 public class Employee {
+	private String name; // 姓名
+	private int salary;  // 薪資 
 	
 	// 建構子
-	public Employee() {
+	public Employee(String name, int salary) {
 		System.out.println("呼叫  Employee 建構子");
+		this.name = name;
+		this.salary = salary;
 	}
+	
+	// 將物件的屬性資料輸出
+	public String toString() {
+		return String.format("name: %s salary: %d", name, salary);
+	}
+	
+	
 }
--- JavaOCP/src/day09/EmployeeMain.java
+++ JavaOCP/src/day09/EmployeeMain.java
@@ -3,10 +3,11 @@ package day09;
 public class EmployeeMain {
 
 	public static void main(String[] args) {
-		Employee emp1 = new Employee();
-		Employee emp2 = new Employee();
-		new Employee();
-
+		Employee emp1 = new Employee("John", 45000);
+		Employee emp2 = new Employee("Mary", 55000);
+		
+		System.out.println(emp1); // emp1.toString();
+		System.out.println(emp2); // emp1.toString();
 	}
 
 }
