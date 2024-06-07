bb9ab18fa887ab7e577f46f1b18e820c147ac4ca
teacher <teacher@BE-403->
2024-06-07 18:54:25 +0800

    java

---
+++ JavaOCP/src/day09/EmployeeMain.java
@@ -0,0 +1,12 @@
+package day09;
+
+public class EmployeeMain {
+
+	public static void main(String[] args) {
+		Employee emp1 = new Employee();
+		Employee emp2 = new Employee();
+		new Employee();
+
+	}
+
+}
