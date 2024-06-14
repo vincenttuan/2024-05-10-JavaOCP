25a6f3895471d22236aa16661177d790b8c0ce3f
teacher <teacher@BE-403->
2024-06-14 19:37:13 +0800

    java

--- JavaOCP/src/day10/ExtendsTest2.java
+++ JavaOCP/src/day10/ExtendsTest2.java
@@ -4,5 +4,8 @@ public class ExtendsTest2 {
 	public static void main(String[] args) {
 		Employee emp = new Employee("John", 45_000);
 		System.out.println(emp);
+		
+		Manager manager = new Manager("Mary", 8_8000, 10_0000);
+		System.out.println(manager);
 	}
 }
--- JavaOCP/src/day10/Manager.java
+++ JavaOCP/src/day10/Manager.java
@@ -2,7 +2,14 @@ package day10;
 
 public class Manager extends Employee {
 	private int budget;
-
+	
+	public Manager(String name, int salary, int budget) {
+		//super(name, salary); // 調用父建構子
+		setName(name);
+		setSalary(salary);
+		this.budget = budget;
+	}
+	
 	public Manager() {
 		System.out.println("建立 Manager 物件");
 	}
@@ -17,7 +24,7 @@ public class Manager extends Employee {
 
 	@Override
 	public String toString() {
-		return "Manager [budget=" + budget + "]";
+		return "Manager [name=" + getName() + ", salary=" + getSalary() + ", budget=" + budget + "]";
 	}
 	
 	
