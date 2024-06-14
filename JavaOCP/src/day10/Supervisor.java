8dda93fb1490b3403f811e1fb6f6f0a61e0ad727
teacher <teacher@BE-403->
2024-06-14 19:45:56 +0800

    java

--- JavaOCP/src/day10/Supervisor.java
+++ JavaOCP/src/day10/Supervisor.java
@@ -2,7 +2,12 @@ package day10;
 
 public class Supervisor extends Manager {
 	private double stockOptions;
-
+	
+	public Supervisor(String name, int salary, int budget, double stockOptions) {
+		super(name, salary, budget);
+		this.stockOptions = stockOptions;
+	}
+	
 	public Supervisor() {
 		System.out.println("建立 Supervisor 物件");
 	}
@@ -17,7 +22,7 @@ public class Supervisor extends Manager {
 
 	@Override
 	public String toString() {
-		return "Supervisor [stockOptions=" + stockOptions + "]";
+		return "Supervisor [name=" + getName() + ", salary=" + getSalary() + ", budget=" + getBudget() + ", stockOptions=" + stockOptions + "]";
 	}
 
 	
