f3e564b68d0944ed3754acb3ecc32cc5a8732e0e
teacher <teacher@BE-403->
2024-06-17 18:51:28 +0800

    java

---
+++ JavaOCP/src/day11/Animal.java
@@ -0,0 +1,8 @@
+package day11;
+
+public class Animal {
+	// 動物叫聲
+	public void sound() {
+		System.out.println("動物的叫聲");
+	}
+}
---
+++ JavaOCP/src/day11/Tiger.java
@@ -0,0 +1,5 @@
+package day11;
+
+public class Tiger extends Cat {
+
+}
