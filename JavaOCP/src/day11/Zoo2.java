bf41cc3dd111c54124844703efd898515b7d4052
teacher <teacher@BE-403->
2024-06-17 21:22:33 +0800

    java

---
+++ JavaOCP/src/day11/Zoo2.java
@@ -0,0 +1,28 @@
+package day11;
+
+// 多型的應用
+public class Zoo2 {
+
+	public static void main(String[] args) {
+//		Dog dog = new Dog();
+//		Cat cat = new Cat();
+//		Tiger tiger = new Tiger();
+		
+//		Animal dog = new Dog();
+//		Animal cat = new Cat();
+//		Animal tiger = new Tiger();
+		
+		// var 自動推斷 
+		var dog = new Dog(); // 相當於 Dog dog = new Dog();
+		var cat = new Cat(); // 相當於 Cat cat = new Cat();
+		var tiger = new Tiger(); // 相當於 Tiger tiger = new Tiger();
+		
+		Animal[] animals = {dog, cat, tiger};
+		
+		for(var animal : animals) {
+			animal.sound();
+		}
+		
+	}
+
+}
