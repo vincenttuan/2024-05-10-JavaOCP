8df69bdd88998e1344473945cac1f8fe1f0ba20e
teacher <teacher@BE-403->
2024-06-17 19:41:27 +0800

    java

---
+++ JavaOCP/src/day11/Coffee.java
@@ -0,0 +1,28 @@
+package day11;
+
+public class Coffee {
+	private String type;
+	private char size;
+	
+	// 建構子超載
+	public Coffee() {
+		type = "美式";
+		size = '大';
+	}
+	
+	public Coffee(String type, char size) {
+		this.type = type;
+		this.size = size;
+	}
+	
+	// 方法超載
+	public void makeCoffee() {
+		System.out.printf("我要 1 杯 %c%s%n", size, type);
+	}
+	
+	public void makeCoffee(int amount) {
+		System.out.printf("我要 %d 杯 %c%s%n", amount, size, type);
+	}
+	
+	
+}
