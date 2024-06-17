755f59f2957d1725f8c9968879c8c96ebabeb170
teacher <teacher@BE-403->
2024-06-17 19:03:19 +0800

    java

--- JavaOCP/src/day11/Dog.java
+++ JavaOCP/src/day11/Dog.java
@@ -1,5 +1,8 @@
 package day11;
 
 public class Dog extends Animal {
-
+	@Override
+	public void sound() {
+		System.out.println("旺旺");
+	}
 }
---
+++ JavaOCP/src/day11/Zoo.java
@@ -0,0 +1,9 @@
+package day11;
+
+public class Zoo {
+	public static void main(String[] args) {
+		Dog dog = new Dog();
+		dog.sound();
+		
+	}
+}
