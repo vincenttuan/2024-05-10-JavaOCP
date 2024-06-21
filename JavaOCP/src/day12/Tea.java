f482520e607676afa9833d921025821a13314fdb
teacher <teacher@BE-403->
2024-06-21 19:25:11 +0800

    java

---
+++ JavaOCP/src/day12/Tea.java
@@ -0,0 +1,25 @@
+package day12;
+
+public class Tea {
+	private String name;
+	private int price;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	@Override
+	public String toString() {
+		return "Tea [名稱=" + name + ", 價格=" + price + "]";
+	}
+	
+}
