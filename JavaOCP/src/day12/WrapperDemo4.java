071fff567e52942502c8f5fd69eb174c3fd9fb11
teacher <teacher@BE-403->
2024-06-21 20:03:31 +0800

    java

---
+++ JavaOCP/src/day12/WrapperDemo4.java
@@ -0,0 +1,29 @@
+package day12;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class WrapperDemo4 {
+	public static void main(String[] args) {
+		int[] scoresA = {
+				Integer.valueOf(100), // auto-unboxing
+				Integer.valueOf(90),  // auto-unboxing
+				Integer.valueOf(80)   // auto-unboxing
+		};
+		
+		Integer[] scoresB = {100, 90, 80}; // auto-boxing
+		
+		// 利用合併串流計算總合
+		int sum = Stream.concat(
+							Arrays.stream(scoresA).boxed(), // 變為 Integer 串流 
+							Arrays.stream(scoresB))
+						.mapToInt(Integer::intValue)
+						.sum();
+		
+		System.out.println(sum);
+				
+				
+				
+		
+	}
+}
