6c70c1e8c0f7de5cd17c549f7bbac442da2b77b6
teacher <teacher@BE-403->
2024-06-21 19:29:07 +0800

    java

---
+++ JavaOCP/src/day12/Milk.java
@@ -0,0 +1,25 @@
+package day12;
+
+public class Milk {
+	private String name;
+	private Integer price;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getPrice() {
+		return price;
+	}
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+	
+	@Override
+	public String toString() {
+		return "Milk [名稱=" + name + ", 價格=" + price + "]";
+	}
+	
+}
