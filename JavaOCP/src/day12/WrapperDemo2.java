f5de99df5bef2fa9ba6e2f1683850d1158eda628
teacher <teacher@BE-403->
2024-06-21 19:38:56 +0800

    java

--- JavaOCP/src/day12/WrapperDemo2.java
+++ JavaOCP/src/day12/WrapperDemo2.java
@@ -7,6 +7,7 @@ public class WrapperDemo2 {
 		tea.setName("綠茶");
 		Milk milk = new Milk();
 		milk.setName("鮮奶");
+		milk.setPrice(10); // auto-boxing, milk.setPrice(Integer.valueOf(10));
 		
 		System.out.println(tea);
 		System.out.println(milk);
