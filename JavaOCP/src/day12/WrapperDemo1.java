6c7f7b3801346fea8831ba9e225692dafc881d10
teacher <teacher@BE-403->
2024-06-21 19:21:58 +0800

    java

--- JavaOCP/src/day12/WrapperDemo1.java
+++ JavaOCP/src/day12/WrapperDemo1.java
@@ -12,7 +12,18 @@ public class WrapperDemo1 {
 		System.out.println(z);
 		int sum = Integer.sum(x, y); // 取得x+y的總和
 		System.out.println(sum); 
-
+		// 使用外包類來運算(Java 5.0以前)
+		Integer a = Integer.valueOf(50); // Integer 物件
+		Integer b = Integer.valueOf(60); // Integer 物件
+		int c = a.intValue() + b.intValue(); // 計算 a + b
+		//      取出a的int值     取出b的int值
+		System.out.println(c);
+		// 使用外包類來運算(Java 5.0以後)
+		Integer d = 50; // auto-boxing 相當於 Integer d = Integer.valueof(50);
+		Integer e = 60; // auto-boxing 相當於 Integer e = Integer.valueof(60);
+		int f = d + e; // auto-unboxing 相當於 int f = d.intValue() + e.intValue();
+		System.out.println(f);
+		
 	}
 
 }
