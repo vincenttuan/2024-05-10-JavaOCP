3e1cc86d586b7f563dbc3c4b01edbf663170181b
teacher <teacher@BE-403->
2024-06-24 20:13:28 +0800

    java

---
+++ JavaOCP/src/day13_2/Latte.java
@@ -0,0 +1,9 @@
+package day13_2;
+
+public class Latte extends AbstractCoffee {
+
+	public Latte(String name, String origin, double price, int strength, String description) {
+		super(name, origin, price, strength, description);
+	}
+
+}
