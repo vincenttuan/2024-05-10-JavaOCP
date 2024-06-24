c5a3a9fda789b77b0e49dddc30cf79562c29235b
teacher <teacher@BE-403->
2024-06-24 20:12:44 +0800

    java

---
+++ JavaOCP/src/day13_2/Espresso.java
@@ -0,0 +1,9 @@
+package day13_2;
+
+public class Espresso extends AbstractCoffee {
+
+	public Espresso(String name, String origin, double price, int strength, String description) {
+		super(name, origin, price, strength, description);
+	}
+	
+}
