efe35d6c3ad9505f4e12c2cf480501515944eebe
teacher <teacher@BE-403->
2024-06-24 21:28:40 +0800

    java

--- JavaOCP/src/day13_3/Fighter.java
+++ JavaOCP/src/day13_3/Fighter.java
@@ -1,7 +1,11 @@
 package day13_3;
 
 public class Fighter extends Airplane implements Weapon {
-
+	
+	public Fighter() {
+		System.out.println("Fighter");
+	}
+	
 	@Override
 	public void shoot() {
 		System.out.println("發射響尾蛇飛彈");
