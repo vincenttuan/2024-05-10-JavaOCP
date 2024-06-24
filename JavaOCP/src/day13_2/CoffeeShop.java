ad3c53827a8e8ed3781b9a55b7afe1582a162fb5
teacher <teacher@BE-403->
2024-06-24 20:14:06 +0800

    java

---
+++ JavaOCP/src/day13_2/CoffeeShop.java
@@ -0,0 +1,22 @@
+package day13_2;
+
+import day13.Coffee;
+import day13.Espresso;
+import day13.Latte;
+
+public class CoffeeShop {
+
+	public static void main(String[] args) {
+		Coffee espresso = new Espresso("Espresso", "衣索比亞", 100, 5, "濃黑咖啡");
+		Coffee latte = new Latte("Latte", "哥倫比亞", 150.5, 3, "熟順滑口");
+		
+		Coffee[] coffees = {espresso, latte};
+		for(Coffee coffee : coffees) {
+			System.out.printf("%s %s %.1f %d %s%n", 
+					coffee.getName(), coffee.getOrigin(), coffee.getPrice(),
+					coffee.getStrength(), coffee.getDescription());
+		}
+ 
+	}
+
+}
