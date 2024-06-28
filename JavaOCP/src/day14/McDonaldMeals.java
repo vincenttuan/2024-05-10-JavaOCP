9c57ae3785e39390a8a8e066e737aa0446cae68e
teacher <teacher@BE-403->
2024-06-28 21:19:52 +0800

    java

--- JavaOCP/src/day14/McDonaldMeals.java
+++ JavaOCP/src/day14/McDonaldMeals.java
@@ -6,8 +6,8 @@ public class McDonaldMeals {
 		Burger fish = new Burger("麥香魚", 45);
 		Burger buger = new Burger("小漢堡", 30);
 		
-		SideMeal fries = new SideMeal("大薯", 60, 3);
-		SideMeal salad = new SideMeal("沙拉", 50, 2);
+		SideMeal fries = new SideMeal("大薯條", 60, 3);
+		SideMeal salad = new SideMeal("沙拉吧", 50, 2);
 		SideMeal onion = new SideMeal("洋蔥圈", 40, 1);
 		
 		Beverage cola = new Beverage("可樂", 20, false);
