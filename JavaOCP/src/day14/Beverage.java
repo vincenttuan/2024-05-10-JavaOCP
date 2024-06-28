b11e9738ab904413c4b887b27b9a68034c36cb46
teacher <teacher@BE-403->
2024-06-28 19:53:09 +0800

    java

--- JavaOCP/src/day14/Beverage.java
+++ JavaOCP/src/day14/Beverage.java
@@ -3,12 +3,19 @@ package day14;
 public class Beverage extends AbstractMeal {
  
 	private Boolean hot = true;
-	 
+	
+	public Beverage(String name, Integer price, Boolean hot) {
+		setName(name);
+		setPrice(price);
+		setHot(hot);
+	}
+	
 	public Boolean isHot() {
-		return null;
+		return hot;
 	}
 	 
 	public void setHot(Boolean hot) {
+		this.hot = hot;
 	}
 	 
 }
