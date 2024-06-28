98c660f27182c2c2a88ba4e7160d21722f7d3ea4
teacher <teacher@BE-403->
2024-06-28 20:51:52 +0800

    java

---
+++ JavaOCP/src/day14/ClassicCombo.java
@@ -0,0 +1,10 @@
+package day14;
+
+// 經典組合餐
+public class ClassicCombo extends Combo {
+
+	public ClassicCombo(Burger burger, SideMeal sideMeal, Beverage beverage) {
+		super(burger, sideMeal, beverage);
+	}
+
+}
