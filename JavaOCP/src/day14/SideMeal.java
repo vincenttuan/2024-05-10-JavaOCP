3bf87305be9ba31c2fab15f7c2580862a3e6d071
teacher <teacher@BE-403->
2024-06-28 19:52:07 +0800

    java

--- JavaOCP/src/day14/SideMeal.java
+++ JavaOCP/src/day14/SideMeal.java
@@ -3,12 +3,19 @@ package day14;
 public class SideMeal extends AbstractMeal {
  
 	private Integer size = 1;
-	 
+	
+	public SideMeal(String name, Integer price, Integer size) {
+		setName(name);
+		setPrice(price);
+		setSize(size);
+	}
+	
 	public Integer getSize() {
-		return null;
+		return size;
 	}
 	 
 	public void setSize(Integer size) {
+		this.size = size;
 	}
 	 
 }
