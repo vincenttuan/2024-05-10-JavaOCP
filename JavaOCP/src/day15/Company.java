f66b053ea163f0a0b1c41b5b057a2438e0c98e65
teacher <teacher@BE-403->
2024-07-01 19:57:39 +0800

    java

--- JavaOCP/src/day15/Company.java
+++ JavaOCP/src/day15/Company.java
@@ -8,6 +8,13 @@ public class Company {
 		this.name = name; // name 區域變數
 	}
 	
+	// 靜態內部類別
+	static class CompanyCEO {
+		static void showCEO() {
+			System.out.println("CEO 是 John");
+		}
+	}
+	
 	public void project(int workdays) {
 		// 方法內部類別-特別專案
 		class SpecialProject {
