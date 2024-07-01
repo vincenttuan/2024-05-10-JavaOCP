a56bbb2b6bc01c14235feb951b967bca8b6e6070
teacher <teacher@BE-403->
2024-07-01 19:34:48 +0800

    java

--- JavaOCP/src/day15/Company.java
+++ JavaOCP/src/day15/Company.java
@@ -8,6 +8,18 @@ public class Company {
 		this.name = name; // name 區域變數
 	}
 	
+	public void project() {
+		// 方法內部類別-特別專案
+		class SpecialProject {
+			void handle() {
+				System.out.printf("執行 %s 特別專案%n", name);
+			}
+		}
+		SpecialProject project = new SpecialProject();
+		project.handle();
+		//...
+	}
+	
 	// 內部類別-部門
 	class Department {
 		private String deptName;
