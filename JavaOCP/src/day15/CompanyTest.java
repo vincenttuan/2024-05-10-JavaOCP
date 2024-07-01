0a6599db96647f20e8831249346558583b7afb2a
teacher <teacher@BE-403->
2024-07-01 19:25:16 +0800

    java

---
+++ JavaOCP/src/day15/CompanyTest.java
@@ -0,0 +1,14 @@
+package day15;
+
+public class CompanyTest {
+
+	public static void main(String[] args) {
+		Company company = new Company("Nvidia");
+		Company.Department deptSales = company.new Department("Sales");
+		Company.Department deptIT = company.new Department("IT");
+		
+		deptSales.displayDeptInfo();
+		deptIT.displayDeptInfo();
+	}
+
+}
