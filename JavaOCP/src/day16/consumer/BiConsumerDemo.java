1015478a4be689392b0ebd2fd2333534c2c4624a
teacher <teacher@BE-403->
2024-07-05 19:38:14 +0800

    java

--- JavaOCP/src/day16/consumer/BiConsumerDemo.java
+++ JavaOCP/src/day16/consumer/BiConsumerDemo.java
@@ -9,10 +9,20 @@ public class BiConsumerDemo {
 		BiConsumer<Integer, Integer> printInfo3 = (x, y) -> System.out.printf("總合: %d%n", (x+y));
 		
 		printData("小明", 90, printInfo1);
+		printData("小米", "大米", printInfo2);
+		printData(100, 90, printInfo3);
 	}
 	
 	// 印出 data 根據 biConsumer 的邏輯
 	public static void printData(String data1, Integer data2, BiConsumer<String, Integer> biConsumer) {
+		biConsumer.accept(data1, data2);
+	}
+	
+	public static void printData(String data1, String data2, BiConsumer<String, String> biConsumer) {
+		biConsumer.accept(data1, data2);
+	}
+	
+	public static void printData(Integer data1, Integer data2, BiConsumer<Integer, Integer> biConsumer) {
 		biConsumer.accept(data1, data2);
 	}
 }
