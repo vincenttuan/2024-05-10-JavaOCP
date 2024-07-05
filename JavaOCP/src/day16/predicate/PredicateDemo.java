62ae21f0d8ef2b231f8645bb768227cda80bb98e
teacher <teacher@BE-403->
2024-07-05 21:17:35 +0800

    java

---
+++ JavaOCP/src/day16/predicate/PredicateDemo.java
@@ -0,0 +1,17 @@
+package day16.predicate;
+
+import java.util.function.Predicate;
+/**
+@FunctionalInterface
+public interface Predicate<T> {
+	boolean test(T t);
+}
+*/
+public class PredicateDemo {
+
+	public static void main(String[] args) {
+		// 利用 Predicate 判斷分數是否及格 ?
+
+	}
+
+}
