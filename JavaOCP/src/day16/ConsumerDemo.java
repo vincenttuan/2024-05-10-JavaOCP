e0fce19b5391db3533937d3174af68dd703e7d1a
teacher <teacher@BE-403->
2024-07-05 19:00:06 +0800

    java

---
+++ JavaOCP/src/day16/ConsumerDemo.java
@@ -0,0 +1,18 @@
+package day16;
+
+import java.util.function.Consumer;
+
+/**
+@FunctionalInterface
+public interface Consumer<T> {
+    void accept(T t);
+} 
+*/
+public class ConsumerDemo {
+
+	public static void main(String[] args) {
+		// Consumer
+
+	}
+
+}
