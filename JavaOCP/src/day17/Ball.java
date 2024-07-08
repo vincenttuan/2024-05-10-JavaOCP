4892996062ba706484c97d3d0066437f17639d4f
teacher <teacher@BE-403->
2024-07-08 18:52:52 +0800

    java

---
+++ JavaOCP/src/day17/Ball.java
@@ -0,0 +1,17 @@
+package day17;
+
+public class Ball {
+	private String color;
+	private int price;
+	
+	public Ball(String color, int price) {
+		this.color = color;
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Ball [color=" + color + ", price=" + price + "]";
+	}
+	
+}
