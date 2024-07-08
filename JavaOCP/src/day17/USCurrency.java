7c63df781095b780326966e062a9d7941f92a820
teacher <teacher@BE-403->
2024-07-08 19:41:03 +0800

    java

---
+++ JavaOCP/src/day17/USCurrency.java
@@ -0,0 +1,16 @@
+package day17;
+
+public enum USCurrency {
+	
+	PENNY(1), NICKLE(5), DIME(10), QUARTER(25);
+	
+	private int value;
+	
+	private USCurrency(int value) {
+		this.value = value;
+	}
+	
+	public int getValue() {
+		return value;
+	}
+}
---
+++ JavaOCP/src/day17/USCurrencyDemo.java
@@ -0,0 +1,10 @@
+package day17;
+
+public class USCurrencyDemo {
+
+	public static void main(String[] args) {
+		USCurrency usCurrency = USCurrency.NICKLE;
+		System.out.println(usCurrency.getValue());
+	}
+
+}
