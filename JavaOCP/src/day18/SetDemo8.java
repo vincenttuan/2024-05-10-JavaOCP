839217efe84cdeade251923569234c947b8b4761
teacher <teacher@BE-403->
2024-07-12 21:36:31 +0800

    java

--- JavaOCP/src/day18/SetDemo8.java
+++ JavaOCP/src/day18/SetDemo8.java
@@ -1,5 +1,6 @@
 package day18;
 
+import java.util.Comparator;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
@@ -22,6 +23,13 @@ public class SetDemo8 {
 		System.out.println(exams);
 		
 		// 3.最後根據分數由大到小印出 (例如:Java:90, VB:80, C#:70)
+		// [Exam [subject=Java, score=90], Exam [subject=VB, score=80], [Exam [subject=C#, score=70]]
+		// 建立一個 exams2 集合來存放由大到小的結果
+		// 此集合的 Comparator 需自行實現由大到小的邏輯並放到 TreeSet 建構子當中
+		Comparator<Exam> comparator = (o1, o2) -> o2.getScore() - o1.getScore();
+		SortedSet<Exam> exams2 = new TreeSet<>(comparator);
+		exams2.addAll(exams); // 將 exams 加入
+		System.out.println(exams2);
 		
 		
 
