976076c99d79507cfb06c3c960300b1a8fc28fc6
teacher <teacher@BE-403->
2024-07-12 21:04:39 +0800

    java

--- JavaOCP/src/day18/SetDemo7.java
+++ JavaOCP/src/day18/SetDemo7.java
@@ -1,13 +1,13 @@
 package day18;
 
-import java.util.Set;
+import java.util.SortedSet;
 import java.util.TreeSet;
 
 public class SetDemo7 {
 
 	public static void main(String[] args) {
 		// Set 排序集合: 使用 TreeSet
-		Set<Integer> tree = new TreeSet<>();
+		SortedSet<Integer> tree = new TreeSet<>();
 		tree.add(5);
 		tree.add(4);
 		tree.add(7);
@@ -17,8 +17,20 @@ public class SetDemo7 {
 		tree.add(1);
 		System.out.println(tree);
 		
+		SortedSet<Integer> sub = tree.subSet(4, 8);
+		System.out.println(sub);
 		
-
+		SortedSet<Integer> tail = tree.tailSet(5);
+		System.out.println(tail);
+		
+		SortedSet<Integer> head = tree.headSet(5);
+		System.out.println(head);
+		
+		System.out.println(tree.first());
+		System.out.println(tree.last());
+		
+		tree.removeIf(n -> n % 2 == 0); // 將偶數元素移除
+		System.out.println(tree);
 	}
 
 }
