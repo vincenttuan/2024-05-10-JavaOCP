22dd259069699070770939605b49f6a502c760c4
teacher <teacher@BE-403->
2024-07-12 20:50:31 +0800

    java

---
+++ JavaOCP/src/day18/SetDemo7.java
@@ -0,0 +1,24 @@
+package day18;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class SetDemo7 {
+
+	public static void main(String[] args) {
+		// Set 排序集合: 使用 TreeSet
+		Set<Integer> tree = new TreeSet<>();
+		tree.add(5);
+		tree.add(4);
+		tree.add(7);
+		tree.add(2);
+		tree.add(6);
+		tree.add(9);
+		tree.add(1);
+		System.out.println(tree);
+		
+		
+
+	}
+
+}
