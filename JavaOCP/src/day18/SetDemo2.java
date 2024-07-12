b20612f34ac94418f6a15d905f0866ad93ea8009
teacher <teacher@BE-403->
2024-07-12 18:54:23 +0800

    java

--- JavaOCP/src/day18/SetDemo2.java
+++ JavaOCP/src/day18/SetDemo2.java
@@ -1,10 +1,23 @@
 package day18;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class SetDemo2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Set 元素不重複 
+		// 透過 equals() & hashcode() 的判定
+		Set set1 = new HashSet();
+		set1.add("Java");
+		set1.add("Java");
+		set1.add("OCP");
+		System.out.println(set1);
+		System.out.println("Java".equals("Java"));
+		System.out.println("Java".hashCode());
+		System.out.println("Java".hashCode());
+		System.out.println("Java".equals("OCP"));
+		System.out.println("OCP".hashCode());
 	}
 
 }
