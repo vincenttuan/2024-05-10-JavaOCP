e1112eba0f6809700b0564c0363c6645a7d93b9f
teacher <teacher@BE-403->
2024-07-15 19:08:57 +0800

    java

--- JavaOCP/src/day19/ListDemo.java
+++ JavaOCP/src/day19/ListDemo.java
@@ -6,13 +6,20 @@ import java.util.List;
 public class ListDemo {
 
 	public static void main(String[] args) {
-		List<Integer> list = new LinkedList<>();
-		list.add(90);
+		List<Integer> list = new LinkedList<>(); // 可變集合
+		list.add(90); // 新增元素
 		list.add(80);
 		list.add(0, 70); // 可以指定放入位置
-		
 		System.out.println(list);
 		
+		// 修改元素
+		list.set(1, 99);
+		System.out.println(list);
+		
+		// 刪除元素
+		//list.remove(1); // 刪除指定位置元素
+		list.remove(Integer.valueOf(99)); // 刪除指定元素物件 
+		System.out.println(list);
 	}
 
 }
