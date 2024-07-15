abdc2d15b754d82a219e1f180b5753481ad1ecf0
teacher <teacher@BE-403->
2024-07-15 19:23:08 +0800

    java

--- JavaOCP/src/day19/ListDemo2.java
+++ JavaOCP/src/day19/ListDemo2.java
@@ -1,12 +1,15 @@
 package day19;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 public class ListDemo2 {
-
+	// ArrayList: 新增, 查詢較快 (記憶體用量較小) O(1)
+	// LinkedList: 修改. 刪除較快  (記憶體用量較大)
 	public static void main(String[] args) {
-		List<Integer> list = new LinkedList<>(); // 可變集合
+		//List<Integer> list = new LinkedList<>(); // 可變集合
+		List<Integer> list = new ArrayList<>(); // 可變集合
 		list.add(90); // 新增元素
 		list.add(50);
 		list.add(40);
