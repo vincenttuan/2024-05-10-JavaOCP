826b6164ab9a57b7478f4140cf67b0ee5da98a3b
teacher <teacher@BE-403->
2024-07-15 19:57:45 +0800

    java

---
+++ JavaOCP/src/day19/Coffee.java
@@ -0,0 +1,35 @@
+package day19;
+
+public class Coffee {
+	private String name;
+	private Integer price;
+	
+	public Coffee(String name, Integer price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getPrice() {
+		return price;
+	}
+
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Coffee [name=" + name + ", price=" + price + "]";
+	}
+	
+	
+	
+}
