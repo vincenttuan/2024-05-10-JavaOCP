af4063aa8154febb69569ec80a0248413aee6dad
teacher <teacher@BE-403->
2024-07-19 20:51:55 +0800

    java

--- JavaOCP/src/day20/MapDemo2.java
+++ JavaOCP/src/day20/MapDemo2.java
@@ -8,7 +8,7 @@ import java.util.Set;
 public class MapDemo2 {
 
 	public static void main(String[] args) {
-		Map<String, Integer> map = Map.of("國", 100, "數", 90, "英", 80 ); // ImmutableCollection
+		Map<String, Integer> map = Map.of("國", 100, "數", 90, "英", 80, "社", null); // ImmutableCollection
 		System.out.println(map);
 		//map.put("社", 70); // 不可以新增
 		// 利用 keySet() 可以取出所有的鍵值(keys)
@@ -17,6 +17,11 @@ public class MapDemo2 {
 		// 利用 values() 可以取出所有的內容
 		List<Integer> values = new ArrayList<>(map.values());
 		System.out.println(values);
+		// 取得總分與平均
+		int sum = map.values().stream().mapToInt(x -> x.intValue()).sum();
+		double avg = map.values().stream().mapToInt(Integer::intValue).average().orElse(0);
+		System.out.println(sum);
+		System.out.println(avg);
 	}
 
 }
