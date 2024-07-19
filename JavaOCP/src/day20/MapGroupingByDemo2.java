70a3040f072b5030de4096562dd8fa7f1a1a4f88
teacher <teacher@BE-403->
2024-07-19 21:34:57 +0800

    java

---
+++ JavaOCP/src/day20/MapGroupingByDemo2.java
@@ -0,0 +1,12 @@
+package day20;
+
+import java.util.List;
+
+public class MapGroupingByDemo2 {
+
+	public static void main(String[] args) {
+		List<String> names = List.of("John", "Jo", "Mary", "Jim", "Mark", "Bob", "Anita");
+		// 請利用分組產出 2個英文字母有幾個?, 3個英文字母有幾個? , 4個英文字母有幾個?, 5個英文字母有幾個? 
+	}
+
+}
