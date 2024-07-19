169e83bd8d2b7533c72a96c7cc975b02222d422f
teacher <teacher@BE-403->
2024-07-19 21:11:04 +0800

    java

---
+++ JavaOCP/src/day20/MapGroupingByDemo.java
@@ -0,0 +1,14 @@
+package day20;
+
+import java.util.List;
+
+// 關於資料分組 grouping by
+public class MapGroupingByDemo {
+
+	public static void main(String[] args) {
+		List<String> fruits = List.of("香蕉", "山竺", "山竺", "山竺", "芒果", "榴槤", "芒果", "西瓜", "西瓜");
+		
+
+	}
+
+}
