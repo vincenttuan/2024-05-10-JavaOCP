1bf76d3ae0a485ae02c10bb8bfc51e63644144a4
teacher <teacher@BE-403->
2024-07-22 21:39:30 +0800

    java

--- JavaOCP/src/day21/ExceptionDemo2.java
+++ JavaOCP/src/day21/ExceptionDemo2.java
@@ -5,21 +5,29 @@ import java.util.Scanner;
 
 public class ExceptionDemo2 {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		while (true) {
-			System.out.print("請輸入2組分數:");
-			try {
-				int score1 = sc.nextInt();
-				int score2 = sc.nextInt();
-				System.out.printf("分數: %d 及格: %b%n", score1, score1 >= 60);
-				System.out.printf("分數: %d 及格: %b%n", score2, score2 >= 60);
-				break; // 跳出迴圈
-			} catch (InputMismatchException e) {
-				System.err.println(e);
-				System.err.println("請輸入數字");
-				sc.nextLine(); // 清空緩衝區
-				continue; // 繼續迴圈
+		Scanner sc = null;
+		try {
+			sc = new Scanner(System.in);
+			while (true) {
+				System.out.print("請輸入2組分數:");
+				try {
+					int score1 = sc.nextInt();
+					int score2 = sc.nextInt();
+					System.out.printf("分數: %d 及格: %b%n", score1, score1 >= 60);
+					System.out.printf("分數: %d 及格: %b%n", score2, score2 >= 60);
+					break; // 跳出迴圈
+				} catch (InputMismatchException e) {
+					System.err.println(e);
+					System.err.println("請輸入數字");
+					sc.nextLine(); // 清空緩衝區
+					continue; // 繼續迴圈
+				}
+			}
+		} finally { // 一定保證會執行
+			if(sc != null) {
+				sc.close(); // 關閉
 			}
 		}
+		
 	}
 }
