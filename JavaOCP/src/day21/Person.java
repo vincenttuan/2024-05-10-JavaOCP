0cfb81e1b84628b0e981c9cae38f052eb19620ea
teacher <teacher@BE-403->
2024-07-22 19:22:13 +0800

    java

---
+++ JavaOCP/src/day21/Person.java
@@ -0,0 +1,52 @@
+package day21;
+
+import java.util.Objects;
+
+public class Person {
+	private String name;
+	private Integer age;
+	
+	public Person(String name, Integer age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(age, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		return Objects.equals(age, other.age) && Objects.equals(name, other.name);
+	}
+
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", age=" + age + "]";
+	}
+	
+}
