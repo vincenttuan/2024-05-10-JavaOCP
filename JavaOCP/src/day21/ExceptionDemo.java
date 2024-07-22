ca60a906f521dfa2554991958c8a8700a42a7879
teacher <teacher@BE-403->
2024-07-22 21:14:57 +0800

    java

--- JavaOCP/src/day21/ExceptionDemo.java
+++ JavaOCP/src/day21/ExceptionDemo.java
@@ -1,5 +1,6 @@
 package day21;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ExceptionDemo {
@@ -7,9 +8,16 @@ public class ExceptionDemo {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("請輸入分數:");
-		int score = sc.nextInt();
 		
-		System.out.printf("分數: %d 及格: %b%n", score, score >= 60);
+		try {
+			int score = sc.nextInt();
+			System.out.printf("分數: %d 及格: %b%n", score, score >= 60);
+		} catch (InputMismatchException e) {
+			System.out.println(e);
+			System.out.println(e.getMessage());
+			System.out.println(e.getLocalizedMessage());
+			System.out.println("請輸入數字");
+		}
 		
 		
 
