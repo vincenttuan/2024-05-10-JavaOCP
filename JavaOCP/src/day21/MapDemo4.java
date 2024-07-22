194574a832e4bd6a5d72eab97a01ec19f7b824ff
teacher <teacher@BE-403->
2024-07-22 19:26:34 +0800

    java

---
+++ JavaOCP/src/day21/MapDemo4.java
@@ -0,0 +1,23 @@
+package day21;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class MapDemo4 {
+	public static void main(String[] args) {
+		// 成員
+		Person p1 = new Person("Bob", 25);
+		Person p2 = new Person("Alice", 30);
+		Person p3 = new Person("David", 20);
+		Person p4 = new Person("Charlie", 35);
+		// 成績 Map, 可以根據　Person 的 name 來排序
+		Map<Person, Integer> scores = new TreeMap<>();
+		scores.put(p1, 100);
+		scores.put(p2, 80);
+		scores.put(p3, 50);
+		scores.put(p4, 75);
+		System.out.println(scores);
+		
+		
+	}
+}
