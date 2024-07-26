d7484d054ba68d1a05d1b29811ba888a4295a86a
teacher <teacher@BE-403->
2024-07-26 21:20:48 +0800

    java

---
+++ JavaOCP/src/day22/SelectGuestbook.java
@@ -0,0 +1,12 @@
+package day22;
+
+public class SelectGuestbook {
+
+	public static void main(String[] args) {
+		// 查詢 mysql guestbook
+		String username = "root";
+		String password = "12345678";
+		String dbUrl = "jdbc:mysql://localhost:3306/demo?serverTimezone=UTC&characterEncoding=utf-8&useUnicode=true";
+	}
+
+}
