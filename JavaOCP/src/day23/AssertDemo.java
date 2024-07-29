dd81f314c0e5da76190d62846ec9edc2f8d50dde
teacher <teacher@BE-403->
2024-07-29 20:03:54 +0800

    java

--- JavaOCP/src/day23/AssertDemo.java
+++ JavaOCP/src/day23/AssertDemo.java
@@ -4,7 +4,8 @@ public class AssertDemo {
 
 	public static void main(String[] args) {
 		int x = 10;
-		assert x > 20: "發生 assert 錯誤";
+		// x == 0 不滿足就會發生 assert
+		assert x == 0: "發生 assert 錯誤";
 		
 		System.out.println("程式正常結束");
 	}
