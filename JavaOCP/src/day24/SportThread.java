7fa4df9a58f0c8fb933ba703d35976c819d0c9a9
teacher <teacher@BE-403->
2024-08-02 20:55:29 +0800

    java

--- JavaOCP/src/day24/SportThread.java
+++ JavaOCP/src/day24/SportThread.java
@@ -1,12 +1,13 @@
 package day24;
 
+// 運動類
 public class SportThread {
 	
-	// 跑馬拉松
+	// 跑馬拉松(1000m)
 	public void marathon() {
 		String tName = Thread.currentThread().getName();
 		for(int i=1;i<=1000;i++) {
-			System.out.printf("%s 跑 %d 公里%n", tName, i);
+			System.out.printf("%s 跑 %d 公尺%n", tName, i);
 		}
 	}
 	
