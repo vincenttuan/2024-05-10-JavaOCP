537ed7a6091194a9a67ebabc4dc280302de4bbdd
teacher <teacher@BE-403->
2024-08-02 21:07:45 +0800

    java

--- JavaOCP/src/day24/SportMain.java
+++ JavaOCP/src/day24/SportMain.java
@@ -6,6 +6,9 @@ public class SportMain {
 		SportThread st1 = new SportThread();
 		SportThread st2 = new SportThread();
 		
+		st1.setName("小帥");
+		st2.setName("小美");
+		
 		st1.start();
 		st2.start();
 		
