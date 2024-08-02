bc879acdfb1421dcebf3d89d8f58bbba5109b1fe
teacher <teacher@BE-403->
2024-08-02 19:57:43 +0800

    java

---
+++ JavaOCP/src/day24/after/Product.java
@@ -0,0 +1,21 @@
+package day24.after;
+
+// 商品
+public class Product {
+	private String name;
+	private int price;
+	
+	public Product(String name, int price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+	
+}
