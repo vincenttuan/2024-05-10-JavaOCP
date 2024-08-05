e44c5b19ac365c48a5ff03331ecc4c6c1aba812b
teacher <teacher@BE-403->
2024-08-05 19:43:01 +0800

    java

--- JavaOCP/src/day25/ThreadDemo.java
+++ JavaOCP/src/day25/ThreadDemo.java
@@ -2,7 +2,17 @@ package day25;
 
 public class ThreadDemo {
 	public static void main(String[] args) {
+		// 待執行的工作
+		Runnable r1 = new SendEmail();
+		Runnable r2 = new SaveData();
 		
+		// 建立執行緒 
+		Thread t1 = new Thread(r1, "email-thread"); // 待執行的工作, 執行緒名稱
+		Thread t2 = new Thread(r2, "save-thread");  // 待執行的工作, 執行緒名稱
+		
+		// 啟動執行緒
+		t1.start();
+		t2.start();
 		
 		
 	}
