ab7ff312e2a1d829dc8574521e9b8de48f37ddb0
teacher <teacher@BE-403->
2024-08-05 19:46:43 +0800

    java

--- JavaOCP/src/day25/SendEmail.java
+++ JavaOCP/src/day25/SendEmail.java
@@ -1,11 +1,16 @@
 package day25;
 
 public class SendEmail implements Runnable {
-
+	private String emailAddress;
+	
+	public SendEmail(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+	
 	@Override
 	public void run() {
 		String tName = Thread.currentThread().getName();
-		System.out.println(tName + " 發送信件");
+		System.out.println(tName + " 執行發送信件程式到 " + emailAddress);
 	}
 	
 }
