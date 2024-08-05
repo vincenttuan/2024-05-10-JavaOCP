ccff5acefdb7eeaeacdc4e8d56ca2e6387d0a08f
teacher <teacher@BE-403->
2024-08-05 21:43:07 +0800

    java

--- JavaOCP/src/day25/FatherThread.java
+++ JavaOCP/src/day25/FatherThread.java
@@ -14,10 +14,16 @@ public class FatherThread extends Thread {
 		worker.start();
 		
 		try {
-			worker.join();
-			System.out.println("爸爸開始洗熱水澡");
+			worker.join(3000); // 設定 join() 時間
+			//worker.join();
+			if(worker.isAlive()) {
+				// 工人雖已超時但仍在執行
+				System.out.println("爸爸洗冷水澡1");
+			} else {
+				System.out.println("爸爸開始洗熱水澡");
+			}
 		} catch (InterruptedException e) {
-			System.out.println("爸爸洗冷水澡");
+			System.out.println("爸爸洗冷水澡2");
 			//e.printStackTrace();
 		} finally {
 			System.out.println("爸爸洗完澡了");
