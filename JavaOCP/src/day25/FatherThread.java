7e5529b01d6d6d108c7c93f8b8df8723dafd0ce3
teacher <teacher@BE-403->
2024-08-05 21:27:11 +0800

    java

---
+++ JavaOCP/src/day25/FatherThread.java
@@ -0,0 +1,16 @@
+package day25;
+
+// 父親執行緒
+public class FatherThread extends Thread {
+
+	@Override
+	public void run() {
+		System.out.println("爸爸下班回家");
+		System.out.println("爸爸要去洗澡");
+		System.out.println("爸爸發現沒瓦斯");
+		System.out.println("爸爸請瓦斯工人送瓦斯");
+		
+		
+	}
+	
+}
---
+++ JavaOCP/src/day25/WorkerThread.java
@@ -0,0 +1,19 @@
+package day25;
+
+public class WorkerThread extends Thread {
+
+	@Override
+	public void run() {
+		System.out.println("瓦斯工人送瓦斯");
+		for(int i=1;i<=5;i++) {
+			try {
+				Thread.sleep(1000);
+			} catch (Exception e) {
+				System.out.println("發生意外: " + e);
+			}
+			System.out.printf("瓦斯工人送瓦斯中 %d 秒鐘\t", i);
+		}
+		System.out.println("\n瓦斯工人送瓦斯到家了");
+	}
+
+}
