80c3ad01d59e3712697f8f5376a3ce8628f5aeda
teacher <teacher@BE-403->
2024-08-09 19:33:26 +0800

    java

---
+++ JavaOCP/src/day26/AccountFile.java
@@ -0,0 +1,43 @@
+package day26;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+public class AccountFile {
+	
+	private static final String BALANCE_FILE = "src/day26/balance.txt"; // 餘額檔
+	
+	static { // 類別資源初始化區段
+		Path path = Paths.get(BALANCE_FILE); // 檔案路徑物件
+		if(!Files.exists(path)) { // 若檔案路徑不存在
+			try {
+				// 將 10000 寫入到檔案
+				// TRUNCATE_EXISTING: 如果文件已經存在, 則在寫入之前清空
+				Files.writeString(path, "10000", 
+						StandardOpenOption.CREATE, 
+						StandardOpenOption.WRITE, 
+						StandardOpenOption.TRUNCATE_EXISTING);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	// 讀取餘額
+	private static int readBalance() {
+		Path path = Paths.get(BALANCE_FILE); // 檔案路徑
+		try {
+			// 讀取檔案內容
+			String content = Files.readString(path);
+			// 將內容轉 int
+			return Integer.parseInt(content);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
+	
+}
