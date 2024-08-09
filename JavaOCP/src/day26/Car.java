b8e24d680720d302ecdcf218948a58ec8312c58e
teacher <teacher@BE-403->
2024-08-09 21:03:26 +0800

    java

--- JavaOCP/src/day26/Car.java
+++ JavaOCP/src/day26/Car.java
@@ -1,6 +1,7 @@
 package day26;
 
 import java.util.Random;
+import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 
 public class Car extends Thread {
@@ -15,12 +16,13 @@ public class Car extends Thread {
 	public void run() {
 		String tName = Thread.currentThread().getName();
 		System.out.printf("%s 從台北出發%n", tName);
+		
 		try {
 			Thread.sleep(new Random().nextInt(3000)); // 模擬開車時間
 			System.out.printf("%s 到台中了%n", tName);
 			cyclicBarrier.await(); // 等待其他人
-		} catch (Exception e) {
-			// TODO: handle exception
+		} catch (InterruptedException | BrokenBarrierException e) {
+			e.printStackTrace();
 		}
 		
 		System.out.printf("%s 繼續往高雄出發...%n", tName);
