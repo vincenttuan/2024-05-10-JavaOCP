bb451467c1edc42a3a1fc52cb3f21833548a2fa8
teacher <teacher@BE-403->
2024-08-09 19:44:26 +0800

    java

--- JavaOCP/src/day26/ATM.java
+++ JavaOCP/src/day26/ATM.java
@@ -4,11 +4,12 @@ public class ATM {
 
 	public static void main(String[] args) {
 		// 建立帳戶
-		AccountVar account = new AccountVar(10000);
+		//Account account = new AccountVar(10000);
+		Account account = new AccountFile();
 		// 建立提款工作
 		Withdraw withdraw1 = new Withdraw(account, 7000);
 		Withdraw withdraw2 = new Withdraw(account, 5000);
-		Withdraw withdraw3 = new Withdraw(account, 3000);
+		Withdraw withdraw3 = new Withdraw(account, 1000);
 		// 建立提款執行緒
 		Thread t1 = new Thread(withdraw1, "John");
 		Thread t2 = new Thread(withdraw2, "Mary");
