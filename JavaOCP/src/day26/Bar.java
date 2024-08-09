76c228e3e342aa4c308eab2cb53b61d75a7c5ffa
teacher <teacher@BE-403->
2024-08-09 21:31:08 +0800

    java

---
+++ JavaOCP/src/day26/Bar.java
@@ -0,0 +1,27 @@
+package day26;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+public class Bar extends Thread {
+	private String[] images = {"♠", "♥", "♦", "♣", "🃏", "7", "♤", "♡", "♢", "♧"};
+	public static List<String> list = new ArrayList<>();
+	
+	private CyclicBarrier cyclicBarrier;
+	
+	public void run() {
+		System.out.println("啟動");
+		try {
+			Thread.sleep(3000);
+			int i = new Random().nextInt(images.length);
+			list.add(images[i]);
+			cyclicBarrier.await();
+		} catch (InterruptedException | BrokenBarrierException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Game Over !");
+	}
+}
